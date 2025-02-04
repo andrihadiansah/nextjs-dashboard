27a3a6f70a8c4f4f9a8733ce0bd0993c3482826e
Andri Hadiansah <andrihadiansah@hotmail.com>
2025-02-04 22:17:44 +0700

    Refactor customer page: remove unused imports and streamline data fetching

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,6 @@
 import Pagination from "@/app/ui/invoices/pagination";
-import Search from "@/app/ui/search";
-import { CreateInvoice } from "@/app/ui/invoices/buttons";
-import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import {
-  fetchCustomers,
-  fetchCustomersPage,
-  fetchFilteredCustomers,
-  fetchInvoicesPages,
-} from "@/app/lib/data";
+import { fetchCustomersPage } from "@/app/lib/data";
 import { Metadata } from "next";
 import CustomersTable from "@/app/ui/customers/table";
 
